161f1ebfe16c31602e939ec370ec44689cbe90d6
Simranjit Singh <simrangrewal587@gmail.com>
2017-03-11 14:48:14 +0530

    Update PrimeNo.js

--- PrimeNo.js
+++ PrimeNo.js
@@ -2,3 +2,7 @@ function isPrime(n) {
   //TODO
    return n < 2 ? false : !/^(11+?)\1+$/.test(Array(n + 1).join('1'));
 }
+
+
+
+http://www.noulakaz.net/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
